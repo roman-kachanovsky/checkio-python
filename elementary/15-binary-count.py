5fb7b3e4a479345c149524ffdf324d6dd3718a81
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-01-12 00:07:36 +0300

    Fixed wrong task description

--- elementary/15-binary-count.py
+++ elementary/15-binary-count.py
@@ -1,17 +1,13 @@
-''' 15. --- Number Base --- Simple
+''' 15. --- Binary count --- Simple
 
-You are given a positive number as a string along with the radix for it. 
-Your function should convert it into decimal form. The radix is less than 37 and greater than 1. 
-The task uses digits and the letters A-Z for the strings.
-Watch out for cases when the number cannot be converted. 
-For example: "1A" cannot be converted with radix 9. For these cases your function should return -1.
+You are given a number (a positive integer). 
+You should convert it to the binary format and count how many unities (1) are in the number spelling. 
+For example: 5 = 0b101 contains two unities, so the answer is 2.
 
-Input:              Two arguments. A number as string and a radix as an integer.
-Output:             The converted number as an integer.
-How it is used:     Here you will learn how to work with the various numeral systems and handle exceptions.
-Precondition:       re.match("\A[A-Z0-9]\Z", str_number)
-                    0 < len(str_number) ≤ 10
-                    2 ≤ radix ≤ 36
+Input:              A number as a positive integer.
+Output:             The quantity of unities in the binary form as an integer.
+How it is used:     How to convert a number to the binary form. It can be useful for Computer Science purposes.
+Precondition:       0 < number ≤ 232
 '''
 
 # My solution:
