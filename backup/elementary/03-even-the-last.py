cd1420f10eb878e72a0ac26be3108883b228582b
roman.kachanovsky <roman.kachanovsky@razortheory.com>
2016-11-02 18:28:30 +0300

    Updated structure of repo

---
+++ backup/elementary/03-even-the-last.py
@@ -0,0 +1,46 @@
+''' 03. --- Even the last --- Elementary
+
+You are given an array of integers. You should find the sum of the 
+elements with even indexes (0th, 2nd, 4th...) then multiply this summed 
+number and the final element of the array together. 
+Don't forget that the first element has an index of 0.
+For an empty array, the result will always be 0 (zero).
+
+Input:              A list of integers.
+Output:             The number as an integer.
+How it is used:     Indexes and slices are important elements of coding 
+                    in python and other languages. 
+                    This will come in handy down the road!
+Precondition:       0 <= len(array) <= 20
+                    all(isinstance(x, int) for x in array)
+                    all(-100 < x < 100 for x in array)
+                    
+'''
+
+# My solution:
+def checkio(array):
+    if not array: # (PEP8)
+        return 0
+    return sum(array[::2]) * array[len(array)-1]
+
+# monkshorin's solution:
+def checkio(array):
+    if len(array) == 0: return 0
+    return sum(array[0::2]) * array[-1]
+
+# aggelgian's solution:
+checkio=lambda x: sum(x[::2])*x[-1] if x else 0
+
+''' To remember:
+
+'if array:' sentence is better and more clear than 'if len(array) != 0'
+
+I can use one-line notation for if, for, while statements if I have
+only one instruction.
+
+array[len(array)-1] == array[-1]
+
+Anonymous functions are used when I don't want to clutter of namespaces
+with unnecessary names of functions.
+
+'''
