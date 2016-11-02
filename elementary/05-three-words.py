11cb54eda174ba74c07f2f3e7482ff89b4be7662
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-02 20:44:10 +0300

    Refactored 05-three-words

---
+++ elementary/05-three-words.py
@@ -0,0 +1,38 @@
+""" 05. --- Three words --- Elementary
+
+You are given a string with words and numbers separated
+by whitespaces (one space). The words contains only letters.
+You should check if the string contains three words in succession.
+For example, the string "start 5 one two three 7 end"
+contains three words in succession.
+
+Input:              A string with words.
+Output:             The answer as a boolean.
+How it is used:     This teaches you how to work with strings
+                    and introduces some useful functions.
+Precondition:       The input contains words and/or numbers. There are
+                    no mixed words (letters and digits combined).
+                    0 < len(words) < 100
+
+"""
+
+
+def my_solution(words):
+    lst = words.split(' ')
+    counter = 0
+    for l in lst:
+        if l.isalpha():
+            counter += 1
+            if counter == 3:
+                return True
+        else:
+            counter = 0
+    return False
+
+
+def veky_solution(words):
+    succ = 0
+    for word in words.split():
+        succ = (succ + 1) * word.isalpha()
+        if succ == 3: return True
+    else: return False
