cff1826a53da526e0cc1872638731a38fb974401
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-03 11:20:56 +0300

    Refactored `non unique elements` task

---
+++ solutions/home/non_unique_elements.py
@@ -0,0 +1,28 @@
+""" 01. --- Non-unique Elements --- Elementary
+
+You are given a non-empty list of integers (X). 
+For this task, you should return a list consisting of only 
+the non-unique elements in this list. To do so you will need 
+to remove all unique elements (elements which are contained in 
+a given list only once). When solving this task, do not change 
+the order of the list.  Example: [1, 2, 3, 1, 3] 1 and 3 non-unique 
+elements and result will be [1, 3, 1, 3].
+
+Input:              A list of integers.
+Output:             The list of integers.
+How it is used:     This mission will help you to understand 
+                    how to manipulate arrays, something that is 
+                    the basis for solving more complex tasks. 
+                    The concept can be easily generalized 
+                    for real world tasks. 
+                    For example: if you need to clarify statistics 
+                    by removing low frequency elements (noise).
+Precondition:       0 < len(data) < 1000
+"""
+
+
+def my_solution(data):
+    return [el for el in data if data.count(el) > 1]
+
+
+veky_solution = lambda d: [x for x in d if d.count(x) > 1]
--- tests/home/test_non_unique_elements.py
+++ tests/home/test_non_unique_elements.py
@@ -6,7 +6,11 @@ from solutions.home.non_unique_elements import my_solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        self.assertIsInstance(my_solution([1]), list)
+        self.assertEqual(my_solution([1, 2, 3, 1, 3]), [1, 3, 1, 3])
+        self.assertEqual(my_solution([1, 2, 3, 4, 5]), [])
+        self.assertEqual(my_solution([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5])
+        self.assertEqual(my_solution([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9])
 
 
 if __name__ == '__main___':
