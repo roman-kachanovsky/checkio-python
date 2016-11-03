da80b868160fc5964f9f0094dbf75dc25bda7cc3
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-03 11:28:10 +0300

    Refactored `pawn brotherhood` task

--- tests/home/test_pawn_brotherhood.py
+++ tests/home/test_pawn_brotherhood.py
@@ -6,7 +6,8 @@ from solutions.home.pawn_brotherhood import my_solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        self.assertEqual(my_solution({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}), 6)
+        self.assertEqual(my_solution({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}), 1)
 
 
 if __name__ == '__main___':
