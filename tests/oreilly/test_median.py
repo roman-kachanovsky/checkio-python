d0d336b1b26913d269fc7ea5c29898c9a6de3b26
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-03 16:19:32 +0300

    Refactored `Median` task

--- tests/oreilly/test_median.py
+++ tests/oreilly/test_median.py
@@ -6,7 +6,11 @@ from solutions.oreilly.median import my_solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        self.assertEqual(my_solution([1, 2, 3, 4, 5]), 3)
+        self.assertEqual(my_solution([3, 1, 2, 5, 3]), 3)
+        self.assertEqual(my_solution([1, 300, 2, 200, 1]), 2)
+        self.assertEqual(my_solution([3, 6, 20, 99, 10, 15]), 12.5)
+        self.assertEqual(my_solution(xrange(1000000)), 499999.5)
 
 
 if __name__ == '__main___':
