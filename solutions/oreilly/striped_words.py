95cfa21b1bc58c21bc30b97eb4bdb82197f6d123
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-10 12:53:43 +0300

    Solved `Striped words` task

--- solutions/oreilly/striped_words.py
+++ solutions/oreilly/striped_words.py
@@ -1,9 +1,56 @@
 """ 04. --- Striped Words ---  Simple
 
-Description
+The alphabet contains both vowel and consonant letters
+(yes, we divide the letters).
+
+Vowels -- A E I O U Y
+Consonants -- B C D F G H J K L M N P Q R S T V W X Z
+
+You are given a block of text with different words. These words are
+separated by white-spaces and punctuation marks.
+Numbers are not considered words in this mission (a mix of letters
+and digits is not a word either). You should count the number
+of words (striped words) where the vowels with consonants are
+alternating, that is; words that you count cannot have
+two consecutive vowels or consonants. The words consisting of
+a single letter are not striped -- do not count those. C
+asing is not significant for this mission.
+
+Input:              A text as a string (unicode)
+Output:             A quantity of striped words as an integer.
+How it is used:     This idea in this task is a useful exercise
+                    for linguistic research and analysis. Text processing
+                    is one of the main tools used in the analysis
+                    of various books and languages and can help
+                    translate print text to a digital format.
+Precondition:       The text contains only ASCII symbols.
+                    0 < len(text) < 10**5
 
 """
 
 
-def my_solution(*args):
-    pass
+VOWELS = 'AEIOUY'
+CONSONANTS = 'BCDFGHJKLMNPQRSTVWXZ'
+
+
+def my_solution(text):
+    def _is_stripped(w):
+        previous = None
+        for l in w:
+            if l == previous:
+                return 0
+            else:
+                previous = l
+        return 1
+
+    words = ''.join([ch.upper() if ch.isalpha() or ch.isdigit() else ' ' for ch in text]).split()
+
+    result = [_is_stripped(
+        ''.join(['1' if ch in VOWELS else '0' for ch in word])
+    ) for word in words if len(word) > 1 and not any(ch.isdigit() for ch in word)]
+
+    return sum(result)
+
+
+# TODO: Investigate most clear solution here
+# https://py.checkio.org/mission/striped-words/publications/review/clear/
--- tests/oreilly/test_striped_words.py
+++ tests/oreilly/test_striped_words.py
@@ -6,7 +6,11 @@ from solutions.oreilly.striped_words import my_solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        self.assertEqual(my_solution('My name is ...'), 3)
+        self.assertEqual(my_solution('Hello world'), 0)
+        self.assertEqual(my_solution('A quantity of striped words.'), 1)
+        self.assertEqual(my_solution('Dog,cat,mouse,bird.Human.'), 3)
+        self.assertEqual(my_solution('1st 2a ab3er root rate'), 1)
 
 
 if __name__ == '__main___':
