3decb66ea3795ec0290808424260b0077df5da63
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-10 12:14:43 +0300

    Refactored `Friends` task

--- tests/oreilly/test_friends.py
+++ tests/oreilly/test_friends.py
@@ -1,12 +1,22 @@
 import unittest
 
-from solutions.oreilly.friends import my_solution
+from solutions.oreilly.friends import MyFriends
 
 
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        letter_friends = MyFriends(({'a', 'b'}, {'b', 'c'},
+                                    {'c', 'a'}, {'a', 'c'}))
+        digit_friends = MyFriends(({'1', '2'}, {'3', '1'}))
+
+        self.assertTrue(letter_friends.add({'c', 'd'}))
+        self.assertFalse(letter_friends.add({'c', 'd'}))
+        self.assertTrue(letter_friends.remove({'c', 'd'}))
+        self.assertFalse(digit_friends.remove({'c', 'd'}))
+        self.assertEqual(letter_friends.names(), {'a', 'b', 'c'})
+        self.assertEqual(letter_friends.connected('d'), set())
+        self.assertEqual(letter_friends.connected('a'), {'b', 'c'})
 
 
 if __name__ == '__main___':
