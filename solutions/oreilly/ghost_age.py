70d5640d75ed279865735c2bb8546387f526c570
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-10 14:33:36 +0300

    Solved `Ghost age` task

--- solutions/oreilly/ghost_age.py
+++ solutions/oreilly/ghost_age.py
@@ -1,9 +1,60 @@
 """ 06. --- Ghost Age ---  Simple
 
-Description
+Nicola takes a moment to study the ghosts. He is trying to think
+up a new method to determine just how old these ghosts are.
+He knows that their age is somehow related to their opacity.
+To measure their opacity Nikola uses a scale of 10000 units
+to 0 units, where 10000 is a completely opaque newborn ghost
+(0 years old) and 0 is completely transparent ghost (of an unknown age).
+
+After some experimenting, Nikola thinks he has discovered the
+law of ghostly opacity. On every birthday, a ghost's opacity is reduced
+by a number of units equal to its age when its age is a Fibonacci
+number (Read about this here) or increased by 1 if it is not.
+
+For example:
+A newborn ghost -- 10000 units of opacity.
+1 year -- 10000 - 1 = 9999 (1 is a Fibonacci number).
+2 year -- 9999 - 2 = 9997 (2 is a Fibonacci number).
+3 year -- 9997 - 3 = 9994 (3 is a Fibonacci number).
+4 year -- 9994 + 1 = 9995 (4 is not a Fibonacci number).
+5 year -- 9995 - 5 = 9990 (5 is a Fibonacci number).
+
+Help Nicola write a function which will determine the age of
+a ghost based on its opacity. You are given opacity
+measurements as a number ranging from 0 to 10000 inclusively.
+The ghosts cannot be older than 5000 years as they simply
+disappear after such a long time (really!).
+
+Input:              An opacity measurement as an integer.
+Output:             The age of the ghost as an integer.
+How it is used:     This task was made for some spooky fun!
+                    We hope to see your funny, tricky and
+                    creative solutions!
+Precondition:       age < 5000
+                    0 < opacity <= 10000
 
 """
 
 
-def my_solution(*args):
-    pass
+def my_solution(opacity):
+    def fib(n):
+        a, b = 1, 2
+        for _ in xrange(n):
+            yield a
+            a, b = b, a + b
+
+    initial_opacity = 10000
+    age = 0
+
+    while initial_opacity:
+        age += 1
+        initial_opacity = initial_opacity - age if age in fib(age) else initial_opacity + 1
+
+        if initial_opacity == opacity:
+            return age
+    return 0
+
+
+# TODO: Investigate most clear solution here
+# https://py.checkio.org/mission/ghosts-age/publications/review/clear/
--- tests/oreilly/test_ghost_age.py
+++ tests/oreilly/test_ghost_age.py
@@ -6,7 +6,12 @@ from solutions.oreilly.ghost_age import my_solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        self.assertEqual(my_solution(10000), 0)
+        self.assertEqual(my_solution(9999), 1)
+        self.assertEqual(my_solution(9997), 2)
+        self.assertEqual(my_solution(9994), 3)
+        self.assertEqual(my_solution(9995), 4)
+        self.assertEqual(my_solution(9990), 5)
 
 
 if __name__ == '__main___':
