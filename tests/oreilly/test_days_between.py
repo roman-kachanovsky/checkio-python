6c9ef0ac5e9c75c41577abbc0cdcbfc35129d24a
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2016-11-10 11:54:26 +0300

    Refactored `Days Between` task

--- tests/oreilly/test_days_between.py
+++ tests/oreilly/test_days_between.py
@@ -6,7 +6,9 @@ from solutions.oreilly.days_between import my_solution
 class TestSolution(unittest.TestCase):
 
     def test_solution(self):
-        pass
+        self.assertEqual(my_solution((1982, 4, 19), (1982, 4, 22)), 3)
+        self.assertEqual(my_solution((2014, 1, 1), (2014, 8, 27)), 238)
+        self.assertEqual(my_solution((2014, 8, 27), (2014, 1, 1)), 238)
 
 
 if __name__ == '__main___':
